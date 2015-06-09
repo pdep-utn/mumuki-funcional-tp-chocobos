a4b235a163f95d786e9294b84c7cdffdda6a232b
Tobias Sandler <toby.s92@gmail.com>
2015-06-08 23:35:16 -0300

    Agrego los extra.hs

---
+++ 0060_elMasWinner/extra.hs
@@ -0,0 +1,14 @@
+mayorSegun f valor1 valor2 = f valor1 > f valor2
+menorSegun f valor1 valor2 = f valor1 < f valor2
+
+elMejorDelTramo (100, f1) apocalipsis = "Gise"
+elMejorDelTramo (50, f2) apocalipsis = "Mati"
+elMejorDelTramo (120, f2) apocalipsis = "Mati"
+elMejorDelTramo (200, f1) apocalipsis = "Gise"
+elMejorDelTramo (80, f3) apocalipsis = "Gise"
+
+elMejorDelTramo (40, f2) apocalipsis = "Alf"
+elMejorDelTramo (90, (\(Chocobo _ (f,p,v))-> f + p + v)) apocalipsis = "Mati"
+elMejorDelTramo (120, fuerza) apocalipsis = "Leo"
+elMejorDelTramo (20, fuerza) apocalipsis = "Leo"
+
