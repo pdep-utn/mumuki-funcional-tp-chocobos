d6400913d05422f9dc35b196e9d405d83c250bff
asanzo <alfredo.sanzo@gmail.com>
2015-06-10 12:58:46 -0300

    Update extra.hs from chocobos excercise 0040
    Added default funcionts

--- 0040_podio/extra.hs
+++ 0040_podio/extra.hs
@@ -1,7 +1,43 @@
+-- ----------------------------------------------------------------
+-- FUNCIONES ENUNCIADO
+-- ----------------------------------------------------------------
+data Chocobo = CChocobo String (Integer,Integer,Integer) deriving (Show)
+
+amarillo = CChocobo "amarillo" (5,3,3)
+negro = CChocobo "negro" (4,4,4)
+blanco = CChocobo "blanco" (2,3,6)
+rojo = CChocobo "rojo" (3,3,4)
+
+fuerza (CChocobo _ (f,_,_)) = f
+peso (CChocobo _ (_,p,_)) = p 
+velocidad (CChocobo _ (_,_,v)) = v 
+
+bosqueTenebroso = [ (100, f1) , (50, f2) , (120, f2) , (200, f1) , (80, f3) ]
+pantanoDelDestino = [ (40, f2) , (90, (\(CChocobo _ (f,p,v))-> f + p + v)) , (120, fuerza) , (20, fuerza) ]
+
+f1 chocobo = velocidad chocobo * 2
+f2 chocobo = velocidad chocobo + fuerza chocobo
+f3 chocobo = velocidad chocobo `div` peso chocobo
+
+data Jinete = CJinete String Chocobo deriving (Show)
+
+leo = CJinete "Leo" amarillo
+gise = CJinete "Gise" blanco
+mati = CJinete "Mati" negro
+alf = CJinete "Alf" rojo
+
+nombre (CJinete n _) = n
+chocobo (CJinete _ ch) = ch 
+
+apocalipsis = [leo, gise, mati, alf]
+
+quickSort _ [] = []
+quickSort criterio (x:xs) =(quickSort criterio . filter (not . criterio x)) xs ++ [x] ++ (quickSort criterio . filter (criterio x)) xs
+
+-- ----------------------------------------------------------------
+-- ----------------------------------------------------------------
 mayorSegun f valor1 valor2 = f valor1 > f valor2
 menorSegun f valor1 valor2 = f valor1 < f valor2
-
-data Chocobo = CChocobo String (Integer, Integer, Integer)
 
 tiempoTotal bosqueTenebroso (CChocobo "amarillo" (5,3,3)) = 150
 tiempoTotal bosqueTenebroso (CChocobo "negro" (4,4,4)) = 138
