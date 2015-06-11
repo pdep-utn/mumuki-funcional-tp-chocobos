c6440152a173707aa4c542ae76a0b2322c530181
tsandler <toby.s92@gmail.com>
2015-06-11 15:07:47 -0300

    Extras files errors fixed

--- 0040_podio/extra.hs
+++ 0040_podio/extra.hs
@@ -1,12 +1,28 @@
+
 mayorSegun f valor1 valor2 = f valor1 > f valor2
 menorSegun f valor1 valor2 = f valor1 < f valor2
 
-tiempoTotal bosqueTenebroso (CChocobo "amarillo" (5,3,3)) = 150
-tiempoTotal bosqueTenebroso (CChocobo "negro" (4,4,4)) = 138
-tiempoTotal bosqueTenebroso (CChocobo "blanco" (2,3,6)) = 85
-tiempoTotal bosqueTenebroso (CChocobo "rojo" (3,3,4)) = 141
+tiempoTotal _ (CChocobo "amarillo" (5,3,3)) = 41
+tiempoTotal _ (CChocobo "negro" (4,4,4)) = 47
+tiempoTotal _ (CChocobo "blanco" (2,3,6)) = 83
+tiempoTotal _ (CChocobo "rojo" (3,3,4)) = 60
 
-tiempoTotal pantanoDelDestino (CChocobo "amarillo" (5,3,3)) = 41
-tiempoTotal pantanoDelDestino (CChocobo "negro" (4,4,4)) = 47
-tiempoTotal pantanoDelDestino (CChocobo "blanco" (2,3,6)) = 83
-tiempoTotal pantanoDelDestino (CChocobo "rojo" (3,3,4)) = 60
+tiempo (CChocobo "amarillo" (5,3,3)) (40, _) = 5
+tiempo (CChocobo "amarillo" (5,3,3)) (90, _) = 8
+tiempo (CChocobo "amarillo" (5,3,3)) (120, _) = 24
+tiempo (CChocobo "amarillo" (5,3,3)) (20, _) = 4
+
+tiempo (CChocobo "negro" (4,4,4)) (40, _) = 5
+tiempo (CChocobo "negro" (4,4,4)) (90, _) = 7
+tiempo (CChocobo "negro" (4,4,4)) (120, _) = 30
+tiempo (CChocobo "negro" (4,4,4)) (20, _) = 5
+
+tiempo (CChocobo "blanco" (2,3,6)) (40, _) = 5
+tiempo (CChocobo "blanco" (2,3,6)) (90, _)  = 8
+tiempo (CChocobo "blanco" (2,3,6)) (120, _) = 60
+tiempo (CChocobo "blanco" (2,3,6)) (20, _) = 10
+
+tiempo (CChocobo "rojo" (3,3,4)) (40, _) = 5
+tiempo (CChocobo "rojo" (3,3,4)) (90, _) = 9
+tiempo (CChocobo "rojo" (3,3,4)) (120, _) = 40
+tiempo (CChocobo "rojo" (3,3,4)) (20, _) = 6
--- 0060_elMasWinner/extra.hs
+++ 0060_elMasWinner/extra.hs
@@ -1,4 +1,4 @@
-mayorSegun f valor1 valor2 = f valor1 > f valor2
+{-mayorSegun f valor1 valor2 = f valor1 > f valor2
 menorSegun f valor1 valor2 = f valor1 < f valor2
 
 data Chocobo = CChocobo String (Integer,Integer,Integer) 
@@ -20,5 +20,15 @@ elMejorDelTramo (80, f3) apocalipsis = "Gise"
 elMejorDelTramo (40, f2) apocalipsis = "Alf"
 elMejorDelTramo (90, f4) apocalipsis = "Mati"
 elMejorDelTramo (120, fuerza) apocalipsis = "Leo"
-elMejorDelTramo (20, fuerza) apocalipsis = "Leo"
+elMejorDelTramo (20, fuerza) apocalipsis = "Leo"-}
+
+
+mayorSegun f valor1 valor2 = f valor1 > f valor2
+menorSegun f valor1 valor2 = f valor1 < f valor2
+
+elMejorDelTramo (100, _) apocalipsis = "Gise"
+elMejorDelTramo (50, _) apocalipsis = "Mati"
+elMejorDelTramo (120, _) apocalipsis = "Mati"
+elMejorDelTramo (200, _) apocalipsis = "Gise"
+elMejorDelTramo (80, _) apocalipsis = "Gise"
 
