dc739fe4bb96da6099f4b39b01640e3844c843b7
tsandler <toby.s92@gmail.com>
2015-06-11 15:14:29 -0300

    One extra.hs left

--- 0060_elMasWinner/extra.hs
+++ 0060_elMasWinner/extra.hs
@@ -1,27 +1,3 @@
-{-mayorSegun f valor1 valor2 = f valor1 > f valor2
-menorSegun f valor1 valor2 = f valor1 < f valor2
-
-data Chocobo = CChocobo String (Integer,Integer,Integer) 
-data Jinete = CJinete String Chocobo
-
-leo = CJinete "Leo" amarillo
-gise = CJinete "Gise" blanco
-mati = CJinete "Mati" negro
-alf = CJinete "Alf" rojo
-
-apocalipsis = [leo, gise, mati, alf]
-
-elMejorDelTramo (100, f1) apocalipsis = "Gise"
-elMejorDelTramo (50, f2) apocalipsis = "Mati"
-elMejorDelTramo (120, f2) apocalipsis = "Mati"
-elMejorDelTramo (200, f1) apocalipsis = "Gise"
-elMejorDelTramo (80, f3) apocalipsis = "Gise"
-
-elMejorDelTramo (40, f2) apocalipsis = "Alf"
-elMejorDelTramo (90, f4) apocalipsis = "Mati"
-elMejorDelTramo (120, fuerza) apocalipsis = "Leo"
-elMejorDelTramo (20, fuerza) apocalipsis = "Leo"-}
-
 
 mayorSegun f valor1 valor2 = f valor1 > f valor2
 menorSegun f valor1 valor2 = f valor1 < f valor2
