d78c58638b729720002182f0afd34c0b115fc8a4
Nahuel Palumbo <nahuel.palumbo@gmail.com>
2015-06-16 21:57:29 -0300

    Se agrega el chocobo amarillo al extra

--- 0030_tiempoTotal/extra.hs
+++ 0030_tiempoTotal/extra.hs
@@ -5,3 +5,7 @@ tiempo (CChocobo "negro" (4,4,4)) (120, _) = 15
 tiempo (CChocobo "negro" (4,4,4)) (200, _) = 25
 tiempo (CChocobo "negro" (4,4,4)) (80, _) = 80
 
+tiempo (CChocobo "amarillo" (5,3,3)) (40, _) = 5
+tiempo (CChocobo "amarillo" (5,3,3)) (90, _) = 8
+tiempo (CChocobo "amarillo" (5,3,3)) (120, _) = 24
+tiempo (CChocobo "amarillo" (5,3,3)) (20, _) = 4
